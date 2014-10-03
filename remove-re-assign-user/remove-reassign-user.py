fecd324178fdfd3ef9bab3a1d9335d2c70b864b6
Kelly Gerrow <kgerrow@esri.com>
2014-10-03 10:33:11 -0700

    reassign
    almost done

--- remove-re-assign-user/remove-reassign-user.py
+++ remove-re-assign-user/remove-reassign-user.py
@@ -33,9 +33,21 @@ def accountInfo():
     URL= 'http://www.arcgis.com/sharing/rest/portals/self?f=json&token=' + token
     response = requests.get(URL)
     jres = json.loads(response.text)
-    return jres['urlKey']
+    return jres
 
-def verifyUser(delUser):
+def userInfo():
+    url ='http://{}.maps.arcgis.com/sharing/rest/community/users/{}'.format(URLKey,remUser)
+    request = url +"?f=json&token="+token
+    response = requests.get(request)
+    jres = json.loads(response.text)
+    userDict = {'fullName':jres['fullName'], 'firstName':jres['firstName'], 'lastName': jres['lastName'], 'description':jres['description'], 'email':jres['email'], 'userType': jres['userType'], 'access': jres['access'],'role':jres['role'], 'tags':jres['tags'], 'culture':jres['culture'], 'region':jres['region'], 'thumbnail':jres['thumbnail'] }
+    return userDict
+
+
+
+
+
+def verifyUser():
     maxURL ='http://{}.maps.arcgis.com/sharing/rest/portals/self/users'.format(URLKey)
     request = maxURL +"?f=json&token="+token
     response = requests.get(request)
@@ -50,133 +62,180 @@ def verifyUser(delUser):
         response = requests.get(request)
         jres = json.loads(response.text)
         for item in jres['users']:
-            if item['username'].lower() == delUser.lower():
+            if item['username'].lower() == remUser.lower():
                 delUser = item['username']
                 foundUser = True
         start+=number
     return delUser, foundUser
 
-def reassignContent( URLKey, token, protected=True):
+def reassignContent():
     '''Deletes all of a user's content including protected content and content
     stored in additional folders.'''
 
-    itemURL ='http://{}.maps.arcgis.com/sharing/rest/content/users/{}'.format(URLKey, user)
+    itemURL ='http://{}.maps.arcgis.com/sharing/rest/content/users/{}'.format(URLKey, remUser)
     request = itemURL +"?f=json&token="+token
     response = requests.get(request)
     jres = json.loads(response.text)
-
+    defaultFolderLst = []
     if jres['items']:
         for item in jres['items']:
-            itemID= item['id']
-            print "Attempting to re-assign item ID:{}".format(itemID)
-            if item['protected']:
-                if protected:
-                    '''delete protected data'''
-                    unprotectURL = 'http://{}.maps.arcgis.com/sharing/rest/content/users/{}/items/{}/unprotect'.format(URLKey,user,itemID)
-                    data = {'f':'json', 'token':token}
-                    response = sendRequest(unprotectURL,urllib.urlencode(data))
-                    delURL = 'http://{}.maps.arcgis.com/sharing/rest/content/users/{}/items/{}/delete'.format(URLKey,user,itemID)
-                    response = sendRequest(delURL,urllib.urlencode(data))
-
-                else:
-                    print "Cannot delete item id {} because it is protected; the user will not be deleted.".format(item['id'])
-
-            else:
-                '''delete unprotected data'''
-                delURL = 'http://{}.maps.arcgis.com/sharing/rest/content/users/{}/items/{}/delete\n\n'.format(URLKey,user,itemID)
-                data = {'f':'json', 'token':token}
-                response = sendRequest(delURL,urllib.urlencode(data))
-
-    else:
-        print 'No items to delete in the default folder.'
+           raURL='http://{}.maps.arcgis.com/sharing/rest/content/users/{}/items/{}/reassign'.format(URLKey,remUser,item['id'])
+           data = {'targetUsername':user, 'targetFolderName':'/', 'f':'json', 'token':token}
+           tjres = requests.post(raURL, data=data, verify=False).json()
+           defaultFolderLst.append(item['id'])
 
 
     if jres['folders']:
         '''Delete content stored in folders'''
+        folderLst = {'Folders': []}
+
         for folder in jres['folders']:
-            folderID = folder['id']
-            foldercontentURL ='http://{}.maps.arcgis.com/sharing/rest/content/users/{}/{}?f=json&token={}'.format(URLKey, user, folderID, token)
-            response = requests.get(foldercontentURL)
-            jres = json.loads(response.text)
+             foldercontentURL ='http://{}.maps.arcgis.com/sharing/rest/content/users/{}/{}?f=json&token={}'.format(URLKey, remUser, folder['id'], token)
+             response = requests.get(foldercontentURL)
+             jres = json.loads(response.text)
 
-            if jres['items']:
+             #itemlist=[]
+             if jres['items']:
+                itemlist=[]
                 for item in jres['items']:
-                    itemID= item['id']
-                    print "Attempting to delete item ID:{}".format(itemID)
+                    itemlist.append(item['id'])
+                    rcfURL ='http://{}.maps.arcgis.com/sharing/rest/content/users/{}/{}/items/{}/reassign'.format(URLKey,remUser,folder['id'],item['id'])
+                    data = {'targetUsername':user, 'targetFolderName':'/', 'f':'json', 'token':token}
+                    jres = requests.post(rcfURL, data=data, verify=False).json()
 
-                    if item['protected']:
-                        if protected:
-                            '''delete protected data'''
-                            unprotectURL = 'http://{}.maps.arcgis.com/sharing/rest/content/users/{}/{}/items/{}/unprotect'.format(URLKey,user,folderID,itemID)
-                            data = {'f':'json', 'token':token}
-                            response = sendRequest(unprotectURL,urllib.urlencode(data))
-                            delURL = 'http://{}.maps.arcgis.com/sharing/rest/content/users/{}/{}/items/{}/delete'.format(URLKey,user,folderID, itemID)
-                            response = sendRequest(delURL,urllib.urlencode(data))
 
-                        else:
-                            print "Cannot delete item id {} located in folder {} because it is protected; the user will not be deleted.".format(item['id'], folderID)
-                    else:
-                        '''delete unprotected data'''
-                        if item['ownerFolder']:
-                            delURL = 'http://{}.maps.arcgis.com/sharing/rest/content/users/{}/{}/items/{}/delete'.format(URLKey,user,folderID,itemID)
-                            data = {'f':'json', 'token':token}
-                            response = sendRequest(delURL,urllib.urlencode(data))
+                folderLst['Folders'].append({'items':[itemlist],'folderName':folder['title'], 'folderID':folder['id']})
+    return defaultFolderLst, folderLst
 
-            #delete the folder
-            try:
-                folderURL = 'http://{}.maps.arcgis.com/sharing/rest/content/users/{}/{}/delete'.format(URLKey,user,folderID)
-                data = {'f':'json','token':token}
-                response = sendRequest(folderURL,urllib.urlencode(data))
-                print "Folder {} has been deleted.".format(folderID)
 
-            except KeyError:
-                print "Unable to delete folder {}, please check that all of the items were deleted from it first.".format(folderID)
 
-def delGroups(userlist, URLKey, token):
-    for user in userlist:
-        groupURL ='http://{}.maps.arcgis.com/sharing/rest/community/users/{}'.format(URLKey, user)
+def reassignGroups():
+
+        groupURL ='http://{}.maps.arcgis.com/sharing/rest/community/users/{}'.format(URLKey, remUser)
         request = groupURL +"?f=json&token="+token
         response = requests.get(request)
         jres = json.loads(response.text)
-        for row in jres['groups']:
-            if row['id'] != "":
-                delURL = 'http://{}.maps.arcgis.com/sharing/rest/community/groups/{}/delete'.format(URLKey,row['id'])
-                data = {'f':'json',
-                        'token':token}
-                response = sendRequest(delURL,urllib.urlencode(data))
-                print "deleting is a group" + row['id']
+        grouplst=[]
+        for item in jres['groups']:
+            reassignURL = 'http://{}.maps.arcgis.com/sharing/rest/community/groups/{}/reassign'.format(URLKey,item['id'])
+            data = {'f':'json', 'targetUsername': user,'token':token}
+            response=requests.post(reassignURL, data=data).json()
+            print response
+            grouplst.append(item['id'])
+        return grouplst
 
-def delUser(userlist, URLKey, token):
-    for user in userlist:
-        userURL ='http://{}.maps.arcgis.com/sharing/rest/community/users/{}/delete'.format(URLKey, user)
-        data = {'f':'json',
-                'token':token}
-        response = sendRequest(userURL,urllib.urlencode(data))
-        if response['success'] is True:
-            print 'Deleted the following user: ' +user
-        else:
-            print 'user was not deleted'
+
+def delUser():
+       #disable MyEsri Access to completly delete the account
+       updateURL ='https://{}.maps.arcgis.com/sharing/rest/community/users/{}/update'.format(URLKey, remUser)
+       data = {'f':'json','usertype':'arcgisonly','token':token}
+       response = requests.post(updateURL, data=data, verify=False).json()
+
+        #delete username
+       userURL ='http://{}.maps.arcgis.com/sharing/rest/community/users/{}/delete'.format(URLKey, remUser)
+       data = {'f':'json', 'token':token}
+       response=requests.post(userURL, data=data).json()
+       #if response['success'] is True:
+        #    print 'Deleted the following user: ' +remUser
+       #else:
+        #    print 'user was not deleted'
+
+def inviteUser():
+    #invite users from spreadsheet
+    url = 'http://{}.maps.arcgis.com/sharing/rest/portals/self/invite'.format(URLKey)
+    #subject and text for email
+    subject = 'An invitation to join an ArcGIS Online Organization, ' + accountInfo['name'] + '. DO NOT REPLY'
+    text = '<html><body><p>' + user+ ' has invited you to join an ArcGIS Online Organization, ' +accountInfo['name'] + '. Please click this link to join:<br><a href="https://www.arcgis.com/home/signin.html?invitation=@@invitation.id@@">https://www.arcgis.com/home/signin.html?invitation=@@invitation.id@@</a></p><p>If you have difficulty signing in, please email your administrator at hellokitty@hotmail.com. Be sure to include a description of the problem, your username, the error message, and a screenshot.</p><p>For your reference, you can access the home page of the organization here: <br>http://'+URLKey +'.maps.arcgis.com/home/</p><p>This link will expire in two weeks.</p><p style="color:gray;">This is an automated email, please do not reply.</p></body></html>'
+
+    #send invitation without sending an email notification to user
+    invitationlist = '{"invitations":[{"username":"'+remUser+'", "password":"Password123", "firstname":"' + userDict['firstName']+'","lastname":"'+userDict['lastName']+'","fullname":"'+userDict['fullName']+'","email":"'+userDict['email']+'","role":"org_publisher"}]}'
+    data={'subject':subject, 'html':text, 'invitationlist':invitationlist,'f':'json', 'token':token}
+    jres = requests.post(url, data=data, verify=False).json()
+
+    #updates description, Enables My Esri Access and makes account searchable to public
+    userURL ='https://{}.maps.arcgis.com/sharing/rest/community/users/{}/update'.format(URLKey, remUser)
+    data = {'f':'json','usertype': userDict['userType'],'description': userDict['description'], 'access':userDict['access'],'tags':userDict['tags'], 'culture':userDict['culture'], 'region':userDict['region'], 'thumbnail':userDict['thumbnail'],'token':token}
+    response = requests.post(userURL, data=data, verify=False).json()
+
+    updateURL = 'http://{}.maps.arcgis.com/sharing/rest/portals/self/updateUserRole'.format(URLKey)
+    data ={'f':'json', 'token':token ,'user':remUser,'role':userDict['role']}
+    response = requests.post(updateURL, data=data, verify=False).json()
+
+def assignContent(defaultLst,folderLst):
+    if folderLst:
+        for item in folderLst['Folders']:
+            urlFol ='http://{}.arcgis.com/sharing/rest/content/users/{}/createFolder'.format(URLKey,remUser)
+            data = {'title': item['folderName'], 'folderName': item['folderName'],'f':'json', 'token':token}
+            response = requests.post(urlFol, data=data, verify=False).json()
+
+
+##    itemURL ='http://{}.maps.arcgis.com/sharing/rest/content/users/{}'.format(URLKey, remUser)
+##    request = itemURL +"?f=json&token="+token
+##    response = requests.get(request)
+##    jres = json.loads(response.text)
+##    defaultFolderLst = []
+##    if jres['items']:
+##        for item in jres['items']:
+##           raURL='http://{}.maps.arcgis.com/sharing/rest/content/users/{}/items/{}/reassign'.format(URLKey,remUser,item['id'])
+##           data = {'targetUsername':user, 'targetFolderName':'/', 'f':'json', 'token':token}
+##           tjres = requests.post(raURL, data=data, verify=False).json()
+##           defaultFolderLst.append(item['id'])
+##
+##
+##    if jres['folders']:
+##        '''Delete content stored in folders'''
+##        folderLst = {'Folders': []}
+##
+##        for folder in jres['folders']:
+##             foldercontentURL ='http://{}.maps.arcgis.com/sharing/rest/content/users/{}/{}?f=json&token={}'.format(URLKey, remUser, folder['id'], token)
+##             response = requests.get(foldercontentURL)
+##             jres = json.loads(response.text)
+##
+##             #itemlist=[]
+##             if jres['items']:
+##                itemlist=[]
+##                for item in jres['items']:
+##                    itemlist.append(item['id'])
+##                    rcfURL ='http://{}.maps.arcgis.com/sharing/rest/content/users/{}/{}/items/{}/reassign'.format(URLKey,remUser,folder['id'],item['id'])
+##                    data = {'targetUsername':user, 'targetFolderName':'/', 'f':'json', 'token':token}
+##                    jres = requests.post(rcfURL, data=data, verify=False).json()
+##
+##
+##                folderLst['Folders'].append({'items':[itemlist],'folderName':folder['title'], 'folderID':folder['id']})
+##    return defaultFolderLst, folderLst
 
 
 if __name__ == '__main__':
 
     #variables
-    user = 'Karate_Kelly'# raw_input("Admin username:")
-    pw  = 'Browncow1'#raw_input("Password:")
+    user = 'essIsoOne'# raw_input("Admin username:")
+    pw  = 'essIsoOne'#raw_input("Password:")
 
-    delUser= 'AaKanKsha_ess'#raw_input("Which user needs to be removed?")
+    remUser= 'poopy1'#raw_input("Which user needs to be removed?")
+
 
     #get token and URL Key
     token = getToken(user, pw)
-    URLKey = accountInfo()
+    accountInfo = accountInfo()
+    URLKey = accountInfo['urlKey']
 
 
     #get list of users to be removed
-    userCase = verifyUser(delUser)
+    userCase = verifyUser()
+    remUser = userCase[0]
+    userDict= userInfo()
     if userCase[1] == False:
-        break
+        print 'Please restart as the username is not located in this organization'
     else:
-        print userCase[0]
+        lists = reassignContent()
+        defaultLst = lists[0]
+        folderLst=lists[1]
+        grouplst=reassignGroups()
+        delUser()
+        inviteUser()
+        assignContent(defaultLst, folderLst)
+        #assigngroups()
+
 
 
 
